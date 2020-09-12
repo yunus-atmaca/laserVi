573e754a7ddcfa2d2c79b8477085e7a7584d48d2
yunus-atmaca <yunusatmacabm@gmail.com>
2020-09-12 16:45:44 +0300

    text addable panel

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,16 +1,166 @@
 import React from 'react'
-import { View, Text, Dimensions } from 'react-native'
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TextInput
+} from 'react-native'
+import { ScrollView, } from 'react-native-gesture-handler';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const { width, height } = Dimensions.get('window')
 
-export default class Home extends React.Component<any, any>{
+const button = {
+  TEXT: 'text',
+  IMAGE: 'image',
+  NONE: 'none'
+}
+
+export default class Home extends React.Component<any, any> {
+
+  textInputPos: any
+  constructor(props) {
+    super(props)
+
+    this.textInputPos = {}
+
+    this.state = {
+      selectedButton: button.NONE,
+      textInputView: null
+    }
+  }
+
+  _panelClicked = (event) => {
+    //console.debug('_panelClicked')
+
+    console.debug('HERE', event.nativeEvent)
+    if (this.state.selectedButton === button.NONE) {
+      console.debug('NOTHING SELECTED')
+    } else {
+      console.debug(this.state.selectedButton)
+      this.textInputPos = { top: event.nativeEvent.locationY, left: event.nativeEvent.locationX }
+      this.setState({ textInputView: true })
+    }
+  }
+
+  _textClicked = () => {
+    //console.debug('_textClicked')
+
+    if (this.state.selectedButton === button.NONE) {
+      this.setState({ selectedButton: button.TEXT })
+    } else {
+      if (this.state.selectedButton === button.IMAGE) {
+        this.setState({ selectedButton: button.TEXT })
+      } else {
+        this.setState({ selectedButton: button.NONE })
+      }
+    }
+  }
+
+  _imageClicked = () => {
+    //console.debug('_imageClicked')
+
+    if (this.state.selectedButton === button.NONE) {
+      this.setState({ selectedButton: button.IMAGE })
+    } else {
+      if (this.state.selectedButton === button.TEXT) {
+        this.setState({ selectedButton: button.IMAGE })
+      } else {
+        this.setState({ selectedButton: button.NONE })
+      }
+    }
+  }
+
+  _getContainerSize = (button) => {
+    return button === this.state.selectedButton ? 32 : 36
+  }
+
+  _getButtonSize = (button) => {
+    return button === this.state.selectedButton ? 28 : 32
+  }
+
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: '#141414' }}>
-        <View style={{ width: width, height: width, backgroundColor: 'white' }}>
-
-        </View>
-      </View>
+        <TouchableWithoutFeedback onPress={(event) => {
+          this._panelClicked(event)
+        }}>
+          <View style={{ width: width, height: width - 50, backgroundColor: 'white' }}>
+            {
+              this.state.textInputView && (
+                <View style={{
+                  position: 'absolute',
+                  top: this.textInputPos.top,
+                  left: this.textInputPos.left,
+                  borderWidth: 1,
+                  borderColor: 'black'
+                }}>
+                  <TextInput
+                    autoFocus={true}
+                    style={styles.textInput}
+                  />
+                </View>
+              )
+            }
+          </View>
+        </TouchableWithoutFeedback>
+        <ScrollView>
+          <View style={{
+            marginHorizontal: 12,
+            marginTop: 12
+          }}>
+            <View style={{ flexDirection: 'row', height: 72 }}>
+              <View style={styles.buttonContainer}>
+                <TouchableWithoutFeedback onPress={this._textClicked}>
+                  <View style={[styles.button, {
+                    width: this._getContainerSize(button.TEXT),
+                    height: this._getContainerSize(button.TEXT),
+                    backgroundColor: '#bfbfbf',
+                  }]}>
+                    <MaterialCommunityIcons
+                      name={'format-text'}
+                      size={this._getButtonSize(button.TEXT)}
+                      color={'black'} />
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+              <View style={styles.buttonContainer}>
+                <TouchableWithoutFeedback onPress={this._imageClicked}>
+                  <View style={[styles.button, {
+                    width: this._getContainerSize(button.IMAGE),
+                    height: this._getContainerSize(button.IMAGE),
+                    backgroundColor: '#bfbfbf',
+                  }]}>
+                    <MaterialCommunityIcons
+                      name={'image-outline'}
+                      size={this._getButtonSize(button.IMAGE)}
+                      color={'black'} />
+                  </View>
+                </TouchableWithoutFeedback>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </View >
     )
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    paddingTop: 0,
+    paddingBottom: 0,
+  }
+})
