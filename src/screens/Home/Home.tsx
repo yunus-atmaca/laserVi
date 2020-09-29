994282a042dd7cbee455193f22432b1f934fd3b8
yunus-atmaca <yunusatmacabm@gmail.com>
2020-09-29 23:12:46 +0300

    add preview button

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -9,6 +9,7 @@ import {
 import Header from '../../components/Header'
 import Button from '../../components/Button'
 import TextCustomization from '../../components/TextCustomization'
+import { ScrollView } from 'react-native-gesture-handler'
 
 const { width, height } = Dimensions.get('window')
 
@@ -42,29 +43,50 @@ class Home extends React.Component<HomeProps, any>{
   render() {
     return (
       <View style={styles.container}>
-        <Header header={'Panel'} backgroundColor={'#141414'} />
-        <View style={styles.panel}>
+        <ScrollView>
+          <Header header={'Panel'} backgroundColor={'#141414'} />
+          <View style={styles.panel}>
 
-        </View>
-        <View style={styles.addingPanelContainer}>
-          <TextCustomization
-            height={styles.addingPanelContainer.height}
-          />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            iconName={'format-text'}
-            iconColor={this.state.selectedButton === VIEW.TEXT ? '#FF9900' : 'white'}
-            indicatorColor={this.state.selectedButton === VIEW.TEXT ? '#FF9900' : '#141414'}
-            action={this._textClicked}
-          />
-          <Button
-            iconName={'image-outline'}
-            iconColor={this.state.selectedButton === VIEW.IMAGE ? '#FF9900' : 'white'}
-            indicatorColor={this.state.selectedButton === VIEW.IMAGE ? '#FF9900' : '#141414'}
-            action={this._imageClicked}
-          />
-        </View>
+          </View>
+          <View style={styles.addingPanelContainer}>
+            <TextCustomization
+              height={styles.addingPanelContainer.height}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <Button
+              iconName={'format-text'}
+              iconColor={this.state.selectedButton === VIEW.TEXT ? '#FF9900' : 'white'}
+              indicatorColor={this.state.selectedButton === VIEW.TEXT ? '#FF9900' : '#141414'}
+              action={this._textClicked}
+            />
+            <Button
+              iconName={'image-outline'}
+              iconColor={this.state.selectedButton === VIEW.IMAGE ? '#FF9900' : 'white'}
+              indicatorColor={this.state.selectedButton === VIEW.IMAGE ? '#FF9900' : '#141414'}
+              action={this._imageClicked}
+            />
+          </View>
+
+          <View style={{
+            alignSelf: 'center',
+            backgroundColor: '#FF9900',
+            height: 36,
+            width: 144,
+            borderRadius: 18,
+            marginVertical: 18,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            <Text style={{
+              color: 'white',
+              fontSize: 18,
+              fontFamily: 'Roboto-Bold',
+            }}>
+              Preview
+            </Text>
+          </View>
+        </ScrollView>
       </View>
     )
   }
